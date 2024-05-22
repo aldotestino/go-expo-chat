6580108e0500045ddc381299b0a046ed9d052418
aldotestino <aldotestino4@gmail.com>
2024-05-22 21:52:44 +0200

    scroll view in settings

--- app/app/(auth)/settings/index.tsx
+++ app/app/(auth)/settings/index.tsx
@@ -2,7 +2,7 @@ import { useAuth, useUser } from "@clerk/clerk-expo";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Link } from "expo-router";
 import React from "react";
-import { View, SafeAreaView, Pressable } from "react-native";
+import { View, SafeAreaView, Pressable, ScrollView } from "react-native";
 
 import SubmitButton from "@/components/SubmitButton";
 import ThemeSwitch from "@/components/ThemeSwitch";
@@ -30,45 +30,47 @@ function Settings() {
 
   return (
     <SafeAreaView className="flex-1 flex items-center">
-      <View className="flex w-full px-4 gap-4 pt-4 items-center">
-        <Link href="/settings/profile" asChild>
-          <Pressable className="w-full">
-            {({ pressed }) => (
-              <View
-                className={cn(
-                  "border border-muted/40 w-full flex flex-row justify-between items-center p-4 rounded-xl bg-card text-card-foreground",
-                  pressed && "opacity-50",
-                )}
-              >
-                <View className="flex flex-row items-center gap-4">
-                  <Avatar alt="your profile image" className="w-10 h-10">
-                    <AvatarImage source={{ uri: user?.imageUrl }} />
-                    <AvatarFallback>
-                      <Text>{user?.username![0]}</Text>
-                    </AvatarFallback>
-                  </Avatar>
-                  <Text variant="title3">{user?.username}</Text>
+      <ScrollView className="w-full">
+        <View className="flex w-full px-4 gap-4 pt-4 items-center">
+          <Link href="/settings/profile" asChild>
+            <Pressable className="w-full">
+              {({ pressed }) => (
+                <View
+                  className={cn(
+                    "border border-muted/40 w-full flex flex-row justify-between items-center p-4 rounded-xl bg-card text-card-foreground",
+                    pressed && "opacity-50",
+                  )}
+                >
+                  <View className="flex flex-row items-center gap-4">
+                    <Avatar alt="your profile image" className="w-10 h-10">
+                      <AvatarImage source={{ uri: user?.imageUrl }} />
+                      <AvatarFallback>
+                        <Text>{user?.username![0]}</Text>
+                      </AvatarFallback>
+                    </Avatar>
+                    <Text variant="title3">{user?.username}</Text>
+                  </View>
+                  <Ionicons
+                    name="chevron-forward"
+                    size={24}
+                    color={colors.grey}
+                  />
                 </View>
-                <Ionicons
-                  name="chevron-forward"
-                  size={24}
-                  color={colors.grey}
-                />
-              </View>
-            )}
-          </Pressable>
-        </Link>
-        <View className="border border-muted/40 w-full flex flex-row justify-between items-center p-4 rounded-xl bg-card text-card-foreground">
-          <Text variant="body">Dark mode</Text>
-          <ThemeSwitch />
+              )}
+            </Pressable>
+          </Link>
+          <View className="border border-muted/40 w-full flex flex-row justify-between items-center p-4 rounded-xl bg-card text-card-foreground">
+            <Text variant="body">Dark mode</Text>
+            <ThemeSwitch />
+          </View>
+          <SubmitButton
+            title="Sign Out"
+            onPress={onSignOut}
+            loading={signOutLoading}
+            color={colors.destructive}
+          />
         </View>
-        <SubmitButton
-          title="Sign Out"
-          onPress={onSignOut}
-          loading={signOutLoading}
-          color={colors.destructive}
-        />
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- app/app/(auth)/settings/profile.tsx
+++ app/app/(auth)/settings/profile.tsx
@@ -1,7 +1,13 @@
 import { useUser } from "@clerk/clerk-expo";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
-import { Pressable, SafeAreaView, View, TextInput } from "react-native";
+import {
+  Pressable,
+  SafeAreaView,
+  View,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 import {
   Avatar,
@@ -56,40 +62,44 @@ function Profile() {
 
   return (
     <SafeAreaView className="flex-1 flex items-center">
-      <View className="flex w-full px-4 pt-4 gap-6 items-center">
-        <View className="w-full flex gap-4 p-4 rounded-xl bg-card text-card-foreground border border-muted/40">
-          <View className="flex flex-row gap-4">
-            <View className="flex items-center gap-1">
-              <Avatar alt="your profile image" className="w-14 h-14">
-                <AvatarImage source={{ uri: user?.imageUrl }} />
-                <AvatarFallback>
-                  <Text>{user?.username![0]}</Text>
-                </AvatarFallback>
-              </Avatar>
-              <Pressable onPress={onPickImage}>
-                {({ pressed }) => (
-                  <Text className={cn("text-primary", pressed && "opacity-50")}>
-                    Update
-                  </Text>
-                )}
-              </Pressable>
+      <ScrollView className="w-full">
+        <View className="flex w-full px-4 pt-4 gap-6 items-center">
+          <View className="w-full flex gap-4 p-4 rounded-xl bg-card text-card-foreground border border-muted/40">
+            <View className="flex flex-row gap-4">
+              <View className="flex items-center gap-1">
+                <Avatar alt="your profile image" className="w-14 h-14">
+                  <AvatarImage source={{ uri: user?.imageUrl }} />
+                  <AvatarFallback>
+                    <Text>{user?.username![0]}</Text>
+                  </AvatarFallback>
+                </Avatar>
+                <Pressable onPress={onPickImage}>
+                  {({ pressed }) => (
+                    <Text
+                      className={cn("text-primary", pressed && "opacity-50")}
+                    >
+                      Update
+                    </Text>
+                  )}
+                </Pressable>
+              </View>
+              <Text variant="subhead" className="flex-1">
+                Insert your name and (optional) a profile image
+              </Text>
             </View>
-            <Text variant="subhead" className="flex-1">
-              Insert your name and (optional) a profile image
-            </Text>
+            <TextInput
+              onFocus={show}
+              ref={inputRef}
+              className="border-y border-muted/40 py-2 h-10C placeholder:text-gray-500 dark:placeholder:text-gray-200 dark:text-white"
+              placeholder="Username"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={username}
+              onChangeText={setUsername}
+            />
           </View>
-          <TextInput
-            onFocus={show}
-            ref={inputRef}
-            className="border-y border-muted/40 py-2 h-10C placeholder:text-gray-500 dark:placeholder:text-gray-200 dark:text-white"
-            placeholder="Username"
-            autoCapitalize="none"
-            autoCorrect={false}
-            value={username}
-            onChangeText={setUsername}
-          />
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
